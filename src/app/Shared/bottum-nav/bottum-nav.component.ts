48d9b12569e23b3f5fb614b68a6f1958183f37bf
Nathandiv <k.n.hlungwani@gmail.com>
2024-11-26 15:02:36 +0200

    added Navbar content

--- src/app/Shared/bottum-nav/bottum-nav.component.ts
+++ src/app/Shared/bottum-nav/bottum-nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-bottum-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './bottum-nav.component.html',
   styleUrl: './bottum-nav.component.css'
 })
